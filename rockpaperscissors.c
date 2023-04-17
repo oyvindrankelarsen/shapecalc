5464fdbd2fbc80c47b841a59b8ea20940f8d0749
oyvindrankelarsen <larsen@larsen.tk>
2023-04-17 11:56:12 +0200

    2

--- rockpaperscissors.c
+++ rockpaperscissors.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "safeinput.h"
 #include "rockpaperscissors.h"
 
@@ -7,23 +8,49 @@ void Rockpaperscissors()
 {
     while (1)
     {
+        int player = 0, computer = 0;
+
         printf("Sten sax påse\n");
         printf("1. Sten\n");
         printf("2. Sax\n");
         printf("3. Påse\n");
         printf("4. Tillbaks till huvudmeny\n");
 
-        int sel = 0;
-        GetInputInt("Välj:", &sel);
-        if (sel == 1)
+                while (!(GetInputInt("Välj: ", &player)))
+        {
+            printf("Mata bara in tal\n");
+        }
+        if (player == 1)
             printf("Du valde sten\n");
-        else if (sel == 2)
+        else if (player == 2)
             printf("Du valde sax\n");
-        else if (sel == 3)
+        else if (player == 3)
             printf("Du valde påse\n");
-        else if (sel == 4)
-            main();
+        else if (player == 4)
+            break;
         else
             printf("Välj ett alternativ ur menyn\n");
+
+        srand(time(NULL));
+
+        printf("Datorn valde");
+        srand(time(NULL));
+        computer = rand() % 3 + 1;
+        if (computer == 1)
+            printf("sten");
+        else if (computer == 2)
+            printf("sax");
+        else if (computer == 3)
+        {
+            printf("påse");
+        }
+        puts("\n");
+        if (player == computer)
+            printf("Oavgjort!\n\n");
+
+        else if ((player == 1 && computer == 2) || (player == 3 && computer == 1) || (player == 2 && computer == 3))
+            printf("Du vann!\n\n");
+        else
+            printf("Datorn vann!\n\n");
     }
 }
